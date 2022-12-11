8086885690b75b41dffb6c24395904d29d3a6173
Sam Heyes-Burke <heyesburkesamuel@gmail.com>
2022-12-11 20:19:00 +0000

    Added basic CSS and fetched the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from "react";
 
+
 export default function App () {
 
-    const [isPlayed, setIsPlayed] = React.useState(false)
+    const [isPlayed, setIsPlayed] = React.useState(true)
+
+    const [questions, setQuestions] = React.useState([])
 
 
     function startQuiz() {
         setIsPlayed(true)
     }
 
+
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=10")
+        .then(res => res.json())
+        .then(data => setQuestions(data))
+    }, [])
+
+    let data = JSON.stringify( questions )
+
+
+
+
+
+
+
+
     return(
         <main>
             {
-        isPlayed ? 
-        <h1>render this</h1>
-        : 
-        <div className="startText">
-            <h1>Quizzical</h1>
-            <h3>Lets start the quiz 😎</h3>
-            <button onClick={startQuiz}>Start quiz</button>
-        </div>
+            isPlayed ? 
+            <div className="container quiz">
+
+       
+                  <p>{data}</p>
+                <h1>The quiz</h1>
+
+            </div>
+
+            : 
+            <div className="container start">
+
+  
+   
+
+                <div className="startText">
+                    <h1>Quizzical</h1>
+                    <h3>Lets start the quiz 😎</h3>
+                    <button onClick={startQuiz} className="startButton">Start quiz</button>
+                </div>
+
+            </div>
+
              }
          </main>
     )
